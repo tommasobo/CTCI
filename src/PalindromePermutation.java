15d489eab7de2777dcf808156377f4e7b119010e
Tommaso Bonato <tommasobonato95@gmail.com>
2018-04-25 02:42:02 +0200

    Added third and fourth questions

---
+++ src/PalindromePermutation.java
@@ -0,0 +1,46 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by tommaso on 24/04/2018.
+ * Check if a permutation of a string is palindrome
+ */
+public class PalindromePermutation {
+
+    public static void main(String[] args) {
+        String string = "mom mom";
+        System.out.println(IsPalindromePermutation(string));
+
+    }
+
+
+    private static boolean IsPalindromePermutation(String text) {
+
+        char[] textArray = text.toCharArray();
+        HashMap<Character, Integer> mapChar = new HashMap<>();
+        boolean isOdd = false;
+
+        // Check if the string is already a palindrome
+        if (new StringBuilder(text).reverse().toString().equals(text)) {
+            return true;
+        }
+
+        // Create hashmap to store characters count
+        for (char aTextArray : textArray) {
+            mapChar.put(aTextArray, mapChar.getOrDefault(aTextArray, 0) + 1);
+        }
+
+        // Check how many chars are repeated a number of odd times
+        for (Map.Entry<Character, Integer> pair : mapChar.entrySet()) {
+            if (((pair.getValue() % 2) != 0)) {
+                if (isOdd) {
+                    return false;
+                }
+                isOdd = true;
+            }
+        }
+        return isOdd;
+    }
+}
+
+
---
+++ src/URLify.java
@@ -0,0 +1,67 @@
+/**
+ * Created by tomma on 24/04/2018.
+ * Write a method to replace all space in a string with %20.
+ */
+public class URLify {
+
+    public static void main(String[] args) {
+        String textString = "test for you and me        ";
+        int length = 19;
+        char[] text = textString.toCharArray();
+        replace(text, length);
+        replaceInPlace(text, length);
+    }
+
+    private static void replaceInPlace(char[] text, int length) {
+
+        int space = 0;
+        for (int i = 0; i < length; i++) {
+            if (text[i] == ' ') {
+                space++;
+            }
+        }
+
+        int realLength = length + (2 * space);
+        for(int i = length - 1; i >=0; i--) {
+            if (text[i] == ' ') {
+                text[realLength -  1] = '0';
+                text[realLength -  2] = '2';
+                text[realLength -  3] = '%';
+                realLength -= 3;
+            } else {
+                text[realLength - 1 ] = text[i];
+                realLength--;
+            }
+        }
+
+        System.out.println(text);
+
+    }
+
+    private static void replace(char[] text, int length) {
+
+        int space = 0;
+        for (int i = 0; i < length; i++) {
+             if (text[i] == ' ') {
+                 space++;
+             }
+        }
+
+        int realLength = length + (2*space), j = 0;
+        char[] finalArray = new char[realLength];
+
+        for (int i = 0; i < length; i++) {
+            if (text[i] == ' ') {
+                finalArray[j] = '%';
+                finalArray[j+1] = '2';
+                finalArray[j+2] = '0';
+                j += 3;
+            } else {
+                finalArray[j] = text[i];
+                j++;
+            }
+        }
+
+        System.out.println(finalArray);
+    }
+}
